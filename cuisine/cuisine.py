9ef503ca14cea74e2ed531022dc268fe11178a40
Droper <pmunoz@gmail.com>
2016-02-24 14:56:36 -0500

    Using 1 and 2 ngrams

--- cuisine/cuisine.py
+++ cuisine/cuisine.py
@@ -7,6 +7,7 @@ Steps to solve the problem
 """
 
 import pandas
+import re
 from nltk.corpus import wordnet as wn
 
 from sklearn import cross_validation
@@ -18,6 +19,25 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 
+
+def print_score_report(ingredients, classes):
+    """ SPLIT THE DATASET FOR SCORING IT
+     THE REAL FINAL SCORE IS DONE IN THE KAGGLE WEB, HERE
+     IS ONLY A TEST
+     Part the train dataset for training and testing
+     Get score with cross validation
+    """
+
+    X, y = ingredients, classes
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    predictor = LogisticRegression()
+    predictor.fit(X_train, y_train)
+    y_pred = predictor.predict(X_test)
+    print classification_report(y_test, y_pred)
+
+
+
 def singularize_words(dataset):
     """Return the dataset with all the words in singular from"""
 
@@ -26,13 +46,43 @@ def singularize_words(dataset):
     # so we have to iterate over each word in the ingredient, so the string is splitted,
     # and wn.morphy is applied to each word, if the word doen't exist in the nltk corpus
     # then the original word is return. After done with the words they are joined in a new string
-    singular_dataset = dataset.map(
+    new_dataset = dataset.map(
         lambda row: [" ".join(map(                 # map over each word in the ingredient, then join them
             lambda item: wn.morphy(item) or item,  # each word is transformed to its basic form or the same word used
             item.split()))                         # the ingredient is splitted
                      for item in row])             # for each ingredient in the recipe
 
-    return singular_dataset
+    return new_dataset
+
+
+def remove_special_characters(dataset):
+    """Return the dataset with all the ingredients without special characters"""
+
+    special_characters = ['&', "'", '"', "''", "/", '%', "!", "(", ")", "\\", ',', '.']
+
+    new_dataset = dataset
+
+    for special_character in special_characters:
+        new_dataset = new_dataset.map(lambda row: [item.replace(special_character, " ") for item in row])
+
+    return new_dataset
+
+
+def lowercase(dataset):
+    """Return the dataset with all the letters lowercase"""
+
+    new_dataset = dataset.map(lambda row: [item.lower() for item in row])
+
+    return new_dataset
+
+
+def remove_numbers(dataset):
+    """Return the dataset without numbers in the ingredients"""
+
+    new_dataset = dataset.map(lambda row: [re.sub("\d+", "", item) for item in row])
+
+    return new_dataset
+
 
 
 TRAIN_PATH = "/home/pedro/proyectos/data-science/cuisine/train.json"
@@ -47,63 +97,36 @@ train_ingredients = train["ingredients"]
 test_ingredients = test["ingredients"]
 classes = train["cuisine"]
 
+
+
 # CountVector for vectorize the data
-count_vect = CountVectorizer()
+count_vect = CountVectorizer(ngram_range=(1, 2))
 
 # Preprocessing the training data
+train_ingredients = lowercase(train_ingredients)
+train_ingredients = remove_special_characters(train_ingredients)
+train_ingredients = remove_numbers(train_ingredients)
 train_ingredients = singularize_words(train_ingredients)    # Singularize each word
-#train_ingredients = train_ingredients.map(lambda row: [item.replace(" ", "") for item in row])
+train_ingredients = train_ingredients.map(lambda row: [item.replace(" ", "-") for item in row])
 train_ingredients = train_ingredients.map(lambda row: " ".join(row)) #join all the words
 train_counts = count_vect.fit_transform(train_ingredients)   # vectorize the data
 
+# TRAINING DATA REPORT
+print_score_report(train_counts, classes)
 
-#models = [LogisticRegression(), MultinomialNB(fit_prior=False, alpha=0.5),
-#  BernoulliNB(), SVC(), DecisionTreeClassifier() ]
-#models = [LogisticRegression()]
-#final_model = {}
-
-# Iterate over the models to validate with each model
-# The best model will be used to submit
-"""
-for model in models:
-    print model.__class__
-    validations = cross_validation.cross_val_score(model, X, y, scoring='accuracy')
-    print validations
-    avg_validations = sum(validations)/len(validations)
-
-    if len(final_model) > 0:
-        if final_model['avg_validations'] < avg_validations:
-            final_model['model'] = model
-            final_model['avg_validations'] = avg_validations
-    else:
-        final_model['model'] = model
-        final_model['avg_validations'] = avg_validations
-
-print final_model
-"""
-
-# SPLIT THE DATASET FOR SCORING IT
-# THE REAL FINAL SCORE IS DONE IN THE KAGGLE WEB, HERE
-# IS ONLY A TEST
-# Part the train dataset for training and testing
-# Get score with cross validation
-X, y = train_counts, classes
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-logistic = LogisticRegression()
-logistic.fit(X_train, y_train)
-y_pred = logistic.predict(X_test)
-print classification_report(y_test, y_pred)
-
-# END OF THE SCORING
 
 # Preprocessing the test data
+test_ingredients = lowercase(test_ingredients)
+test_ingredients = remove_special_characters(test_ingredients)
+test_ingredients = remove_numbers(test_ingredients)
 test_ingredients = singularize_words(test_ingredients)  # Singularize each word
-#test_ingredients = test_ingredients.map(lambda row: [item.replace(" ", "") for item in row])
+test_ingredients = test_ingredients.map(lambda row: [item.replace(" ", "-") for item in row])
 test_ingredients = test_ingredients.map(lambda row: " ".join(row)) # Join all the words
 test_counts = count_vect.transform(test_ingredients) # vectorize
 
-clf = LogisticRegression().fit(train_counts, classes)
+
+# Do the prediction
+clf = LogisticRegression(multi_class='multinomial', solver='newton-cg').fit(train_counts, classes)
 predictions = clf.predict(test_counts)
 
 
